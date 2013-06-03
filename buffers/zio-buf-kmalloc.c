eliovalenzuela/zio

commit 2300e624cea7dceb91a13a20d2fe1049679e66bb

buffers: properly handle retr_block called by push
buffer->store_block calls trigger->push_block when it receives the
first block. The trigger user fires directly , and if the device
accepts the data immediately, our data_done() will retr_block from the
buffer.  This happens with zio-zero, cset 1, and leads to a deadlock.

To allow immediate success of push_block down to device level, release
the lock before calling push_block. The code here also handles
pathologic situations like two processes storing at the same time.

Signed-off-by: Alessandro Rubini <rubini@gnudd.com>
Acked-by: Federico Vaga <federico.vaga@gmail.com>

buffers/zio-buf-kmalloc.c
@@ -102,15 +102,26 @@ static void zbk_free_block(struct zio_bi *bi, struct zio_block *block)
 }
 
 /* When write() stores the first block, we try pushing it */
-static inline int __try_push(struct zio_ti *ti, struct zio_channel *chan,
+static inline int __try_push(struct zio_bi *bi, struct zio_channel *chan,
 			     struct zio_block *block)
 {
+	struct zio_ti *ti = chan->cset->ti;
+	int pushed;
+
 	/* chek if trigger is disabled */
 	if (unlikely((ti->flags & ZIO_STATUS) == ZIO_DISABLED))
 		return 0;
-	if (ti->t_op->push_block(ti, chan, block) < 0)
-		return 0;
-	return 1;
+	/*
+	 * If push succeeds and the device eats data immediately,
+	 * the trigger may call retr_block right now.  So
+	 * release the lock but also say we can't retrieve now.
+	 */
+	bi->flags |= ZBI_PUSHING;
+	spin_unlock(&bi->lock);
+	pushed = (ti->t_op->push_block(ti, chan, block) == 0);
+	spin_lock(&bi->lock);
+	bi->flags &=  ~ZBI_PUSHING;
+	return pushed;
 }
 
 /* Store is called by the trigger (for input) or by f->write (for output) */
@@ -135,20 +146,20 @@ static int zbk_store_block(struct zio_bi *bi, struct zio_block *block)
 	spin_lock(&bi->lock);
 	if (zbki->nitem == bi->zattr_set.std_zattr[ZATTR_ZBUF_MAXLEN].value)
 		goto out_unlock;
+	list_add_tail(&item->list, &zbki->list);
 	if (!zbki->nitem) {
 		if (unlikely(output))
-			pushed = __try_push(chan->cset->ti, chan, block);
+			pushed = __try_push(bi, chan, block);
 		else
 			awake = 1;
 	}
-	if (likely(!pushed)) {
-		zbki->nitem++;
-		list_add_tail(&item->list, &zbki->list);
-	}
+	if (pushed)
+		list_del(&item->list);
+	zbki->nitem += !pushed;
 	spin_unlock(&bi->lock);
 
-	/* if input, awake user space */
-	if (awake && ((bi->flags & ZIO_DIR) == ZIO_DIR_INPUT))
+	/* if first input, awake user space */
+	if (awake)
 		wake_up_interruptible(&bi->q);
 	return 0;
 
@@ -169,7 +180,7 @@ static struct zio_block *zbk_retr_block(struct zio_bi *bi)
 	zbki = to_zbki(bi);
 
 	spin_lock(&bi->lock);
-	if (list_empty(&zbki->list))
+	if (!zbki->nitem || bi->flags & ZBI_PUSHING)
 		goto out_unlock;
 	first = zbki->list.next;
 	item = list_entry(first, struct zbk_item, list);

buffers/zio-buf-vmalloc.c
@@ -162,15 +162,26 @@ static void zbk_free_block(struct zio_bi *bi, struct zio_block *block)
 }
 
 /* When write() stores the first block, we try pushing it */
-static inline int __try_push(struct zio_ti *ti, struct zio_channel *chan,
+static inline int __try_push(struct zio_bi *bi, struct zio_channel *chan,
 			     struct zio_block *block)
 {
+	struct zio_ti *ti = chan->cset->ti;
+	int pushed;
+
 	/* chek if trigger is disabled */
 	if (unlikely((ti->flags & ZIO_STATUS) == ZIO_DISABLED))
 		return 0;
-	if (ti->t_op->push_block(ti, chan, block) < 0)
-		return 0;
-	return 1;
+	/*
+	 * If push succeeds and the device eats data immediately,
+	 * the trigger may call retr_block right now.  So
+	 * release the lock but also say we can't retrieve now.
+	 */
+	bi->flags |= ZBI_PUSHING;
+	spin_unlock(&bi->lock);
+	pushed = (ti->t_op->push_block(ti, chan, block) == 0);
+	spin_lock(&bi->lock);
+	bi->flags &=  ~ZBI_PUSHING;
+	return pushed;
 }
 
 /* Store is called by the trigger (for input) or by f->write (for output) */
@@ -179,7 +190,7 @@ static int zbk_store_block(struct zio_bi *bi, struct zio_block *block)
 	struct zbk_instance *zbki = to_zbki(bi);
 	struct zio_channel *chan = bi->chan;
 	struct zbk_item *item;
-	int awake = 0, pushed = 0, output;
+	int awake = 0, pushed = 0, output, first;
 
 	pr_debug("%s:%d (%p, %p)\n", __func__, __LINE__, bi, block);
 
@@ -193,20 +204,21 @@ static int zbk_store_block(struct zio_bi *bi, struct zio_block *block)
 
 	/* add to the buffer instance or push to the trigger */
 	spin_lock(&bi->lock);
-	if (list_empty(&zbki->list)) {
+	first = list_empty(&zbki->list);
+	list_add_tail(&item->list, &zbki->list);
+	if (first) {
 		if (unlikely(output))
-			pushed = __try_push(chan->cset->ti, chan, block);
+			pushed = __try_push(bi, chan, block);
 		else
 			awake = 1;
 	}
-	if (likely(!pushed))
-		list_add_tail(&item->list, &zbki->list);
+	if (pushed)
+		list_del(&item->list);
 	spin_unlock(&bi->lock);
 
-	/* if input, awake user space */
-	if (awake && ((bi->flags & ZIO_DIR) == ZIO_DIR_INPUT))
+	/* if first input, awake user space */
+	if (awake)
 		wake_up_interruptible(&bi->q);
-
 	return 0;
 }
 
@@ -222,7 +234,7 @@ static struct zio_block *zbk_retr_block(struct zio_bi *bi)
 	zbki = to_zbki(bi);
 
 	spin_lock(&bi->lock);
-	if (list_empty(&zbki->list))
+	if (list_empty(&zbki->list) || bi->flags & ZBI_PUSHING)
 		goto out_unlock;
 	first = zbki->list.next;
 	item = list_entry(first, struct zbk_item, list);

include/linux/zio-buffer.h
@@ -80,6 +80,12 @@ struct zio_bi {
 };
 #define to_zio_bi(obj) container_of(obj, struct zio_bi, head.dev)
 
+/* first 4bit are reserved for zio object universal flags */
+enum zbi_flag_mask {
+	ZBI_PUSHING = 0x10,	/* a push is being performed */
+};
+
+
 /* The block is the basic data item being transferred */
 struct zio_block {
 	unsigned long		ctrl_flags;

commit 3a82bbc8cc1421f0c536e2f9a95ec378435ee314

bugfix in buffers: block might be used after being freed
With trigger user and output channels, a block may be referenced after
being released. This affects both kmalloc and vmalloc buffers.

This bug was introduced by commit 2300e624, partly reversed by this.
However, we only noticed when running under CONFIG_DEBUG_SLAB.

Bug description:

  When store_block is called for output the first time, it pushes to
  the trigger, but as of commit 2300e624 it adds the block to the
  list first (to keep ordering if another push happens very soon).
  If push succeeds *and* cset->raw_io succeeds immediately, the
  block is freed.  Store_block will thus remove it from the list
  of enqueued blocks, accessing the data structure.

Bug solution:

  The store_block method runs calls trigger->push in locked context,
  like it used to before 2300e624. Thus, the block doesn't need
  to be enqueued (as other store processes are waiting for the
  spinlock) and is not dequeued if push succeeds.

The ZIO_BI_PUSHING flag remains, to prevent the deadlock when
store succeeds and data_done() tries to retrieve the next block.
However, it is checked by retr_block before taking the lock.

Signed-off-by: Alessandro Rubini <rubini@gnudd.com>
Acked-by: Federico Vaga <federico.vaga@gmail.com>

buffers/zio-buf-kmalloc.c
@@ -116,6 +116,12 @@ static void zbk_free_block(struct zio_bi *bi, struct zio_block *block)
 	item = to_item(block);
 	zbki = item->instance;
 
+	/* Special case: the block is freed while pushing: locked context */
+	if (bi->flags & ZIO_BI_PUSHING) {
+		zbki->nitem--;
+		goto out_free;
+	}
+
 	spin_lock_irqsave(&bi->lock, flags);
 	if ( ((bi->flags & ZIO_DIR) == ZIO_DIR_OUTPUT) &&
 	     zbki->nitem < bi->zattr_set.std_zattr[ZIO_ATTR_ZBUF_MAXLEN].value)
@@ -123,6 +129,7 @@ static void zbk_free_block(struct zio_bi *bi, struct zio_block *block)
 	zbki->nitem--;
 	spin_unlock_irqrestore(&bi->lock, flags);
 
+out_free:
 	kfree(block->data);
 	zio_free_control(zio_get_ctrl(block));
 	kmem_cache_free(zbk_slab, item);
@@ -140,15 +147,9 @@ static inline int __try_push(struct zio_bi *bi, struct zio_channel *chan,
 	/* chek if trigger is disabled */
 	if (unlikely((ti->flags & ZIO_STATUS) == ZIO_DISABLED))
 		return 0;
-	/*
-	 * If push succeeds and the device eats data immediately,
-	 * the trigger may call retr_block right now.  So
-	 * release the lock but also say we can't retrieve now.
-	 */
+	/* Keep the lock, but mark we are pushing so trigger-user won't retr */
 	bi->flags |= ZIO_BI_PUSHING;
-	spin_unlock(&bi->lock); /* Don't irqrestore here, keep them disabled */
 	pushed = (ti->t_op->push_block(ti, chan, block) == 0);
-	spin_lock(&bi->lock);
 	bi->flags &=  ~ZIO_BI_PUSHING;
 	return pushed;
 }
@@ -168,15 +169,14 @@ static int zbk_store_block(struct zio_bi *bi, struct zio_block *block)
 	/* add to the buffer instance or push to the trigger */
 	spin_lock_irqsave(&bi->lock, flags);
 	isempty = list_empty(&zbki->list);
-	list_add_tail(&item->list, &zbki->list);
 	if (isempty) {
 		if (unlikely(output))
 			pushed = __try_push(bi, chan, block);
 		else
 			awake = 1;
 	}
-	if (pushed)
-		list_del(&item->list);
+	if (!pushed)
+		list_add_tail(&item->list, &zbki->list);
 	spin_unlock_irqrestore(&bi->lock, flags);
 
 	/* if first input, awake user space */
@@ -196,8 +196,13 @@ static struct zio_block *zbk_retr_block(struct zio_bi *bi)
 
 	zbki = to_zbki(bi);
 
+	/* PUSHING is only active temporarily during locked context */
+	if (bi->flags & ZIO_BI_PUSHING)
+		return NULL;
+
+	/* There is no trig->push in our call trace, proceed to get the lock */
 	spin_lock_irqsave(&bi->lock, flags);
-	if (list_empty(&zbki->list) || bi->flags & ZIO_BI_PUSHING)
+	if (list_empty(&zbki->list))
 		goto out_unlock;
 	first = zbki->list.next;
 	item = list_entry(first, struct zbk_item, list);

buffers/zio-buf-vmalloc.c
@@ -183,15 +183,9 @@ static inline int __try_push(struct zio_bi *bi, struct zio_channel *chan,
 	/* chek if trigger is disabled */
 	if (unlikely((ti->flags & ZIO_STATUS) == ZIO_DISABLED))
 		return 0;
-	/*
-	 * If push succeeds and the device eats data immediately,
-	 * the trigger may call retr_block right now.  So
-	 * release the lock but also say we can't retrieve now.
-	 */
+	/* Keep the lock, but mark we are pushing so trigger-user won't retr */
 	bi->flags |= ZIO_BI_PUSHING;
-	spin_unlock(&bi->lock);
 	pushed = (ti->t_op->push_block(ti, chan, block) == 0);
-	spin_lock(&bi->lock);
 	bi->flags &=  ~ZIO_BI_PUSHING;
 	return pushed;
 }
@@ -215,15 +209,14 @@ static int zbk_store_block(struct zio_bi *bi, struct zio_block *block)
 	/* add to the buffer instance or push to the trigger */
 	spin_lock_irqsave(&bi->lock, flags);
 	first = list_empty(&zbki->list);
-	list_add_tail(&item->list, &zbki->list);
 	if (first) {
 		if (unlikely(output))
 			pushed = __try_push(bi, chan, block);
 		else
 			awake = 1;
 	}
-	if (pushed)
-		list_del(&item->list);
+	if (!pushed)
+		list_add_tail(&item->list, &zbki->list);
 	if (!first && zbki->flags & ZBK_FLAG_MERGE_DATA)
 		zbk_try_merge(zbki, item);
 	spin_unlock_irqrestore(&bi->lock, flags);
@@ -246,8 +239,13 @@ static struct zio_block *zbk_retr_block(struct zio_bi *bi)
 
 	zbki = to_zbki(bi);
 
+	/* PUSHING is only active temporarily during locked context */
+	if (bi->flags & ZIO_BI_PUSHING)
+		return NULL;
+
+	/* There is no trig->push in our call trace, proceed to get the lock */
 	spin_lock_irqsave(&bi->lock, flags);
-	if (list_empty(&zbki->list) || bi->flags & ZIO_BI_PUSHING)
+	if (list_empty(&zbki->list))
 		goto out_unlock;
 	first = zbki->list.next;
 	item = list_entry(first, struct zbk_item, list);